722dcdf097fef687d135ae44d41eb158b002850e
Lisa Henry <henrylis@ie.ibm.com>
2018-03-21 08:43:59 -0400

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="Lisa test 7 --  Hello World ";
+   			dojo.query("span.shareSome-title")[0].textContent="Lisa test 1 --  Hello World ";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
