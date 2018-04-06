6296ad00754e52d484662c2b2ea31224cd7071fa
Mariko <37789376+marimaris@users.noreply.github.com>
2018-04-06 10:34:35 +0900

    Update helloWorld.user.js

--- helloWorld/helloWorld.user.js
+++ helloWorld/helloWorld.user.js
@@ -45,7 +45,7 @@ if(typeof(dojo) != "undefined") {
             waitFor( function(){
 			// wait until the "loading..." node has been hidden
 			// indicating that we have loaded content.
-   			dojo.query("span.shareSome-title")[0].textContent="何かシェアしてみてん";
+   			dojo.query("span.shareSome-title")[0].textContent="何かシェアして下さい";
        	          },
 		  ".lotusStreamTopLoading div.loaderMain.lotusHidden");
       } catch(e) {
